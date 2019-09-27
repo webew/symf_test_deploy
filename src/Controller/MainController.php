8ebe9d784599592c9238a7b73488dccc9332f4c1
webew <fillion.bertrand@gmail.com>
2019-09-27 08:28:36 +0200

    deploiement

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/main", name="main")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
